5b4f7b51d741927b9b1bfb4c1179ea1dfcebad01
senaparlak <senaparlak123@gmail.com>
2026-07-03 20:54:45 +0300

    feat: implement configuration reader

--- src/main/java/com/senaparlak/restassured/config/ConfigReader.java
+++ src/main/java/com/senaparlak/restassured/config/ConfigReader.java
@@ -1,5 +1,24 @@
 package com.senaparlak.restassured.config;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ConfigReader {
 
-}
+    private static final Properties properties = new Properties();
+
+    static {
+        try {
+            FileInputStream file = new FileInputStream("src/test/resources/config/config.properties");
+            properties.load(file);
+        } catch (IOException e) {
+                throw new RuntimeException("Config file could not be loaded", e);
+        }
+        }
+        public static String getProperty(String key) {
+        return properties.getProperty(key);
+        }
+    }
+
